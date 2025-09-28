1b09b9bb65545fb089db18d4624e514945615590
faith <faith.henu@student.moringaschool.com>
2025-09-28 18:34:01 +0300

    learned something new

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 from fastapi import FastAPI
+from enum import Enum
+from typing import Optional
+
 
 app=FastAPI()
 
 @app.get('/')
 def index():
     return 'Hello world'
+
+# @app.post('/')
+# def index2():
+#     return 'hi'
+@app.get('/blog/all')
+def get_all_blogs():
+    return {'message':'all blogs provided'}
+
+@app.get('/blog/new')
+def get_new_blog(page = 1, page_size=10):
+    return{'message':f'all {page_size} blogs on page {page}'}
+
+@app.get('/blog/{id}/comments/{comment_id}')
+def get_comment(id: int, comment_id: int, valid: bool = True, Username: Optional[str] = None):
+    return {'message':f'Blog id {id} , comment_id {comment_id}, valid {valid}, username {Username} '}
+
+class BlogType(str, Enum):
+    short= 'short'
+    story= 'story'
+    howto= 'howto'
+
+@app.get('/blog/type/{BlogType}')
+def get_blog_type(BlogType):
+    return{'message': f'this is the blogtype {BlogType}'}    
+
+@app.get('/blog/{id}')
+def get_blog(id: int):
+    return {'message':f'blog with {id}'}
+
