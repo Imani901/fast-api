69c9c5d7545fa646221c3465ea20f5cd8220873a
faith <faith.henu@student.moringaschool.com>
2025-10-01 10:43:31 +0300

    tags

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ def index():
 # @app.post('/')
 # def index2():
 #     return 'hi'
-@app.get('/blog/all')
+@app.get('/blog/all' ,tags=['blogs'])
 def get_all_blogs():
     return {'message':'all blogs provided'}
 
-@app.get('/blog/new')
+@app.get('/blog/new' ,tags=['blogs'])
 def get_new_blog(page = 1, page_size=10):
     return{'message':f'all {page_size} blogs on page {page}'}
 
-@app.get('/blog/{id}/comments/{comment_id}')
+@app.get('/blog/{id}/comments/{comment_id}', tags=['comments'])
 def get_comment(id: int, comment_id: int, valid: bool = True, Username: Optional[str] = None):
     return {'message':f'Blog id {id} , comment_id {comment_id}, valid {valid}, username {Username} '}
 
@@ -29,11 +29,11 @@ class BlogType(str, Enum):
     story= 'story'
     howto= 'howto'
 
-@app.get('/blog/type/{BlogType}')
+@app.get('/blog/type/{BlogType}' ,tags=['blogs'])
 def get_blog_type(BlogType):
     return{'message': f'this is the blogtype {BlogType}'}    
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK ,tags=['blogs'])
 def get_blog(id: int, response: Response):
     if id>5:
         response.status_code = status.HTTP_404_NOT_FOUND
